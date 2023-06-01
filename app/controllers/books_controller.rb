ba2d6ad235f31ca63a84bab2d9da4305886f0af0
Pakin Kraisin <dxpakin@gmail.com>
2023-06-01 21:11:25 +0700

    feat: delete books (not deleting comments)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -35,6 +35,12 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
   private
 
   # Strong param
