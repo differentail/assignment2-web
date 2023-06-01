906ff4466c139fd60ef26e7168ab201343358db4
Pakin Kraisin <dxpakin@gmail.com>
2023-06-01 15:40:19 +0700

    feat: new book page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,27 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    return unless @book.save
+
+    redirect_to books_path
+  end
+
+  private
+
+  # Strong param
+  # params[:book][:name]
+  # params[:book][:description]
+  # params[:book][:release]
+
+  def book_params
+    params.require(:book).permit(:name, :description, :release)
+  end
 end
