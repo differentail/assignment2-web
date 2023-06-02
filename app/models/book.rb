e4464ab6cc4a3aac76fcbb6ba10779ce109a7cc1
Pakin Kraisin <dxpakin@gmail.com>
2023-06-02 13:35:30 +0700

    refactor: by comments 1-2

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,10 +5,7 @@ class Book < ApplicationRecord
   validates :description, allow_blank: true, presence: true
 
   def summary_star
-    stars = 0.0
-    reviews.each do |review|
-      stars += review.star
-    end
+    stars = reviews.sum(:star)
 
     stars / reviews.count
   end
