1e67ad8d16fc14820d040ec920afd6c9e5eeaec6
Pakin Kraisin <dxpakin@gmail.com>
2023-06-02 11:26:07 +0700

    feat: show reviews

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,13 @@ class Book < ApplicationRecord
 
   validates :name, presence: true
   validates :description, allow_blank: true, presence: true
+
+  def summary_star
+    stars = 0.0
+    reviews.each do |review|
+      stars += review.star
+    end
+
+    stars / reviews.count
+  end
 end
