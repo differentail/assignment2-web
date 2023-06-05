99fb39c88475f2c1e23945d2cb2afc41815fe80d
Pakin Kraisin <dxpakin@gmail.com>
2023-06-05 15:44:33 +0700

    feat: book specs

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: books
@@ -9,3 +11,45 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  describe 'valid factory' do
+    subject(:factory) { build(:book) }
+
+    it { is_expected.to be_valid }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to allow_value('').for(:description) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:reviews) }
+  end
+
+  describe '#summary_star' do
+    subject { book.summary_star }
+    let(:book_a) { create(:book) }
+    let(:book_b) { create(:book) }
+    let(:review_a) { create(:review, book: book_b) }
+    let(:review_b) { create(:review, book: book_b) }
+
+    context 'no reviews' do
+      let(:book) { book_a }
+
+      it 'returns nil' do
+        is_expected.to eq(nil)
+      end
+    end
+
+    context 'general case' do
+      let(:book) { book_b }
+
+      it 'returns average of stars' do
+        is_expected.to eq(book.reviews.sum(:star) / book.reviews.count)
+      end
+    end
+  end
+end
