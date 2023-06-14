e60ae5124f730fcb58ac55ba987f1f253daec554
Pakin Kraisin <dxpakin@gmail.com>
2023-06-14 11:24:28 +0700

    feat: job spec

--- spec/sidekiq/daily_ranking_job_spec.rb
+++ spec/sidekiq/daily_ranking_job_spec.rb
@@ -3,9 +3,26 @@
 require 'rails_helper'
 
 describe DailyRankingJob, type: :job do
-  # it 'should call SomeService' do
-  #   expect(TestService).to receive(:hello)
+  let!(:book1) { create(:book) }
+  let!(:book2) { create(:book) }
+  let!(:book3) { create(:book) }
+  let(:memory_store) { ActiveSupport::Cache.lookup_store(:redis_cache_store) }
+  let(:cache) { Rails.cache }
 
-  #   subject.perform
-  # end
+  before do
+    allow(Rails).to receive(:cache).and_return(memory_store)
+    Rails.cache.clear
+  end
+
+  before do
+    Rails.cache.write("book/#{book1.id}/views", 10, raw: true)
+    Rails.cache.write("book/#{book2.id}/views", 1, raw: true)
+    Rails.cache.write("book/#{book3.id}/views", 90, raw: true)
+  end
+
+  it 'rank all books by views descending' do
+    subject.perform
+    ranking = Rank.first
+    expect(ranking.book_ranks.order(:order_id).to_a.pluck(:book_id)).to eq([3, 1, 2])
+  end
 end
